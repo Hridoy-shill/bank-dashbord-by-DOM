f49f0771e9d06f18bc222bb8d95150c3734ba7d8
Hridoy-shill <ridoyaaa60@gmail.com>
2023-02-13 18:15:39 +0600

    new code added

--- js/withdrow.js
+++ js/withdrow.js
@@ -7,16 +7,20 @@ document.getElementById('widrow-btn').addEventListener('click', function () {
     const previwesWithdrawTotalString = previwesWithdrawTotal.innerText;
     const newWithdrawTotal = parseFloat(previwesWithdrawTotalString);
 
-    const totalWithdrawAmount = newWithdrowAmout + newWithdrawTotal;
-    
-    previwesWithdrawTotal.innerText = totalWithdrawAmount;
-
     withdrawField.value = '';
 
     const totalBalance = document.getElementById('total-balance');
     const totalBalanceString = totalBalance.innerText;
     const currentTotalBalance = parseFloat(totalBalanceString);
-    
+
+    if (newWithdrowAmout > currentTotalBalance) {
+        alert('Non Enaugh Balance!!!')
+        return;
+    }
+
+    const totalWithdrawAmount = newWithdrowAmout + newWithdrawTotal;
+    previwesWithdrawTotal.innerText = totalWithdrawAmount;
+
     const updatedTotalBalanc = currentTotalBalance - newWithdrowAmout;
     totalBalance.innerText = updatedTotalBalanc;
 })
